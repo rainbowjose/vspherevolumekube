56d1221b0ded7862d288b3273d0ffe9fa764e7ca
rainbowjose <youlold@gmail.com>
2020-04-20 00:18:00 +0000

    first commit

---
+++ set.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#VM_NAME=$(kubectl get nodes -o json | jq '.items[]|[.metadata.name]')
+#VM_UUID=$(kubectl get nodes -o json | jq '.items[]|[.status.nodeInfo.systemUUID]')
+PAIRS=$(kubectl get nodes -o json | jq -r -c '.items[]|[.metadata.name, .status.nodeInfo.systemUUID]' | tr -d '[]"' | tr '[:upper:]' '[:lower:]')
+for vm in $PAIRS; do
+	VM_NAME=${vm%,*}
+	VM_UUID=${vm#*,}
+	echo Patching $VM_NAME with $VM_UUID
+	kubectl patch node $VM_NAME -p "{\"spec\":{\"providerID\":\"vsphere://$VM_UUID\"}}"
+done
