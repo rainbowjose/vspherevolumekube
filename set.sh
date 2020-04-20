70ffd9ca906d564a8b5ff2ad6ebb42dd015979c0
rnbj <rnbj@node1.cluster.local>
2020-04-20 21:24:16 +0000

    updated big endian to little endian uuid vsphere

--- set.sh
+++ set.sh
@@ -5,5 +5,5 @@ for vm in $PAIRS; do
 	VM_NAME=${vm%,*}
 	VM_UUID=${vm#*,}
 	echo Patching $VM_NAME with $VM_UUID
-	kubectl patch node $VM_NAME -p "{\"spec\":{\"providerID\":\"vsphere://$VM_UUID\"}}"
+	kubectl patch node $VM_NAME -p "{\"spec\":{\"providerID\":\"vsphere://${v:6:2}${v:4:2}${v:2:2}${v:0:2}-${v:11:2}${v:9:2}-${v:16:2}${v:14:2}${v:18:36}\"}}"
 done
